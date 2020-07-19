3a0af05355eecfef541e59102f863064368d4ab2
Jacob Ruranski <j.ruranski@icloud.com>
2020-07-19 15:21:07 +0200

    Initial Commit

---
+++ CORE/COREApp.swift
@@ -0,0 +1,17 @@
+//
+//  COREApp.swift
+//  CORE
+//
+//  Created by Jakub Ruranski on 19/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct COREApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
