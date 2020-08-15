07932c3a1b70388da34720ada0c4ebe993246329
Jacob Ruranski <j.ruranski@icloud.com>
2020-08-15 10:49:23 +0200

    COREdata added

--- CORE/COREApp.swift
+++ CORE/COREApp.swift
@@ -6,12 +6,51 @@
 //
 
 import SwiftUI
+import CoreData
 
 @main
 struct COREApp: App {
+    
+    let context = PersistentContainer.persistentContainer.viewContext
+    
+    
     var body: some Scene {
         WindowGroup {
-            TabBar()
+            TabBar().environment(\.managedObjectContext, context)
+        }
+    }
+}
+
+
+
+
+public class PersistentContainer {
+    // MARK: - Define Constants / Variables
+    public static var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    // MARK: - Initializer
+    private init() {}
+    // MARK: - Core Data stack
+    public static var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CoreData")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    // MARK: - Core Data Saving support
+    public static func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
         }
     }
 }
